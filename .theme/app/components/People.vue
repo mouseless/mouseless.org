30983cd5be6a1e0c4ab25b4905bd3a10b8e68074
Liva <ahmetlivacengiz@gmail.com>
2025-11-07 16:01:54 +0300

    convert styles to tailwind classes in `People`

--- .theme/app/components/People.vue
+++ .theme/app/components/People.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="people"
-    :class="`people--align_${align}`"
+    class="mt-4 mb-4 w-full"
+    :class="`text-${align}`"
   >
-    <div class="members people__members">
+    <div class="inline-flex flex-row flex-wrap gap-(--space-xs)">
       <div v-for="member in members" :key="member.login">
-        <a :href="member.html_url" target="_blank" class="members__member member">
-          <img :src="member.avatar_url" class="member__image">
+        <a :href="member.html_url" target="_blank">
+          <img :src="member.avatar_url" :class="`w-[${itemHeight}] h-[${itemHeight}] rounded-[${itemHeight}] transition-transform duration-200 ease-[cubic-bezier(0.175,0.885,0.32,2)] hover:scale-150`" >
         </a>
       </div>
     </div>
@@ -31,36 +31,3 @@ const members = ref([]);
 onServerPrefetch(async() => members.value = await getPeople());
 onBeforeMount(async() => members.value = await getPeople());
 </script>
-<style lang="scss" scoped>
-.people {
-  margin-top: 1em;
-  margin-bottom: 1em;
-  width: 100%;
-
-  &--align {
-    &_left { text-align: left; }
-    &_center { text-align: center; }
-    &_right { text-align: right; }
-  }
-
-  &__members {
-    display: inline-flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: var(--space-xs);
-  }
-}
-
-.member {
-  &__image {
-    width: v-bind(itemHeight);
-    height: v-bind(itemHeight);
-    border-radius: v-bind(itemHeight);
-
-    &:hover {
-      transition: scale 0.2s cubic-bezier(0.175, 0.885, 0.32, 2);
-      scale: 1.5;
-    }
-  }
-}
-</style>
