1dcd0922010efc0ba75ee20a03c6030464a52a62
cihandeniz <cihan@gcbrains.com>
2025-11-12 18:14:39 +0100

    add fallback variables for devmode failure of mermaid

--- .theme/app/plugins/mermaid.client.js
+++ .theme/app/plugins/mermaid.client.js
@@ -10,30 +10,30 @@ const mermaidOptions = {
   }
 };
 
-function getCSSVariable(variable) {
-  if(typeof document === "undefined") { return ""; }
+function getCSSVariable(variable, fallback) {
+  if(typeof document === "undefined") { return fallback; }
 
   return getComputedStyle(document.documentElement).getPropertyValue(variable).trim();
 }
 
 const themeVariables = {
-  fontFamily: getCSSVariable("--font-default"),
-  fontSize: getCSSVariable("--font-size"),
-  primaryColor: getCSSVariable("--color-mermaid-primary"),
-  primaryTextColor: getCSSVariable("--color-light-link"),
-  primaryBorderColor: getCSSVariable("--color-mermaid-primary"),
-  lineColor: getCSSVariable("--color-mermaid-line"),
-  secondaryColor: getCSSVariable("--color-mermaid-secondary"),
-  secondaryTextColor: getCSSVariable("--color-light-link"),
-  secondaryBorderColor: getCSSVariable("--color-mermaid-secondary"),
-  tertiaryColor: getCSSVariable("--color-mermaid-tertiary"),
-  activationBkgColor: getCSSVariable("--color-mermaid-primary"),
-  quadrant1Fill: getCSSVariable("--color-mermaid-fill1"),
-  quadrant2Fill: getCSSVariable("--color-mermaid-tertiary"),
-  quadrant3Fill: getCSSVariable("--color-dark-box-bg"),
-  quadrant4Fill: getCSSVariable("--color-mermaid-tertiary"),
-  quadrantInternalBorderStrokeFill: getCSSVariable("--color-mermaid-secondary"),
-  quadrantExternalBorderStrokeFill: getCSSVariable("--color-mermaid-secondary")
+  fontFamily: getCSSVariable("--font-default", "system-ui"),
+  fontSize: getCSSVariable("--font-size", "18px"),
+  primaryColor: getCSSVariable("--color-mermaid-primary", "black"),
+  primaryTextColor: getCSSVariable("--color-light-link", "lightgray"),
+  primaryBorderColor: getCSSVariable("--color-mermaid-primary", "black"),
+  lineColor: getCSSVariable("--color-mermaid-line", "gray"),
+  secondaryColor: getCSSVariable("--color-mermaid-secondary", "darkgray"),
+  secondaryTextColor: getCSSVariable("--color-light-link", "lightgray"),
+  secondaryBorderColor: getCSSVariable("--color-mermaid-secondary", "darkgray"),
+  tertiaryColor: getCSSVariable("--color-mermaid-tertiary", "darkgray"),
+  activationBkgColor: getCSSVariable("--color-mermaid-primary", "black"),
+  quadrant1Fill: getCSSVariable("--color-mermaid-fill1", "black"),
+  quadrant2Fill: getCSSVariable("--color-mermaid-tertiary", "darkgray"),
+  quadrant3Fill: getCSSVariable("--color-dark-box-bg", "black"),
+  quadrant4Fill: getCSSVariable("--color-mermaid-tertiary", "darkgray"),
+  quadrantInternalBorderStrokeFill: getCSSVariable("--color-mermaid-secondary", "darkgray"),
+  quadrantExternalBorderStrokeFill: getCSSVariable("--color-mermaid-secondary", "darkgray")
 };
 
 export default defineNuxtPlugin(() => {
